ac91b532d51745c10ae69f2cfb010f1288374985
Aeolus.Zhang-张敬峥 <Aeolus.Zhang@fanruan.com>
2023-02-03 20:17:46 +0800

    feat: add homepage/1.png and homepage/2.png
    - add homepage/1.png and homepage/2.png
    - add homepage/1.test.js and homepage/2.test.js
    - modify .gitignore
    - modify config.js
    - modify README.md

--- test/homepage/1.test.js
+++ test/homepage/1.test.js
@@ -4,7 +4,7 @@ const { precision } = require('../../config');
 it('homepage/1.png', () => {
     expect.assertions(1);
     return ssim('./sources/main/homepage/1.png', './sources/test/homepage/1.png').then(r => {
-        console.log('最小精确度:',precision,'相似度:',r.ssim,'🐱')
+        console.log('设置最小精确度:',precision,'相似度:',r.ssim,'🐱')
         expect(r.ssim > precision).toBe(true);
     })
 });
--- test/homepage/2.test.js
+++ test/homepage/2.test.js
@@ -4,7 +4,7 @@ const { precision } = require('../../config');
 it('homepage/2.png', () => {
     expect.assertions(1);
     return ssim('./sources/main/homepage/2.png', './sources/test/homepage/2.png').then(r => {
-        console.log('最小精确度:',precision,'相似度:',r.ssim,'🐱')
+        console.log('设置最小精确度:',precision,'相似度:',r.ssim,'🐱')
         expect(r.ssim > precision).toBe(true);
     })
 });
