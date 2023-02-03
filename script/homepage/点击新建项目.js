b07600df7c815ecea5e769d6c497ac140178d8b0
Aeolus.Zhang-张敬峥 <Aeolus.Zhang@fanruan.com>
2023-02-03 20:54:28 +0800

    [TEST] 添加自动登录的代码
    - 添加自动登录的代码
    - 添加自动登录的代码
    - 添加自动登录的代码

--- script/homepage/点击新建项目.js
+++ script/homepage/点击新建项目.js
@@ -9,25 +9,29 @@ const user = key.split('/')[0];
 const password = key.split('/')[1];
 const dest = __filename.replace("/script/", `/sources/${type}/`).replace('.js', '.png');
 
+/**
+ * 这块是自动登录的代码
+ */
+const login = (browser) => {
+    return browser
+        .navigateTo(server)
+        .useXpath() // every selector now must be xpath
+        .click("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div[2]/input")
+        .useCss() // we're back to CSS now
+        .setValue('input[type=text]', user)
+        .useXpath() // every selector now must be xpath
+        .click("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/div[2]/input")
+        .useCss() // we're back to CSS now
+        .setValue('input[type=password]', password)
+        .useXpath()
+        .click('/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[4]/div/div[1]/div[2]/div')
+        .useCss()
+        .pause(2000);
+}
+
 describe('homepage2', function () {
     it('homepage2.test', function () {
-        /**
-         * 这块是自动登录的代码
-         */
-        browser
-            .navigateTo(server)
-            .useXpath() // every selector now must be xpath
-            .click("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div[2]/input")
-            .useCss() // we're back to CSS now
-            .setValue('input[type=text]', user)
-            .useXpath() // every selector now must be xpath
-            .click("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/div[2]/input")
-            .useCss() // we're back to CSS now
-            .setValue('input[type=password]', password)
-            .useXpath()
-            .click('/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[4]/div/div[1]/div[2]/div')
-            .useCss()
-            .pause(2000)
+        login(browser)
             /**
              * 下面是登录后操作的内容
              */
