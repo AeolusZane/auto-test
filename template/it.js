897ad63afe88367af2d352bce954d18201e972c8
Aeolus.Zhang-张敬峥 <Aeolus.Zhang@fanruan.com>
2023-02-03 20:06:18 +0800

    feat: add test
    - add test
    - add test

--- template/it.js
+++ template/it.js
@@ -1,7 +1,7 @@
 it('<%name%>', () => {
     expect.assertions(1);
     return ssim('<%main%>', '<%test%>').then(r => {
-        console.log('最小精确度:',precision,'相似度:',r.ssim,'🐱')
+        console.log('设置最小精确度:',precision,'相似度:',r.ssim,'🐱')
         expect(r.ssim > precision).toBe(true);
     })
 });
